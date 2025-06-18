d267d97a21c0ee07b4371716e530404062df9e46
Kevinishd <72179447+Kevinishd@users.noreply.github.com>
2025-06-18 21:09:21 +0800

    Update header.php
    修改标题文字；增加 meta data 让链接预览时具有标题、简介以及缩略图

--- header.php
+++ header.php
@@ -26,6 +26,61 @@ if ($GLOBALS['style_BG'] != '') {
                 'author'    =>  _t('%s')
             ), '', ' - '); ?><?php $this->options->title(); ?></title>
 
+    <!-- Open graph-->
+    <meta property="og:site_name" content="<?php $this->options->title() ?>" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="<?php $this->permalink() ?>" />
+    <meta property="og:title" content="<?php $this->archiveTitle(array(
+                'category'  =>  _t('%s'),
+                'search'    =>  _t('%s'),
+                'tag'       =>  _t('%s'),
+                'author'    =>  _t('%s')
+            ), '', ' - '); ?><?php $this->options->title(); ?>" />
+    <meta property="og:description" content="<?php $this->description(); ?>" />
+    <meta property="og:image" content="<?php echo getPostFirstImg($this->cid); ?>" />
+    <meta property="og:category" content="<?php $this->category(',', false); ?>" />
+    <meta property="article:author" content="<?php $this->author(); ?>" />
+    <meta property="article:publisher" content="<?php $this->options->siteUrl(); ?>" />
+    <meta property="article:published_time" content="<?php $this->date('c'); ?>" />
+    <meta property="article:published_first" content="<?php $this->options->title() ?>, <?php $this->permalink() ?>" />
+    <meta property="article:tag" content="<?php $this->keywords(',');?>" />
+    
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@kevinishd">
+    <meta name="twitter:title" content="<?php $this->archiveTitle(array(
+                'category'  =>  _t('%s'),
+                'search'    =>  _t('%s'),
+                'tag'       =>  _t('%s'),
+                'author'    =>  _t('%s')
+            ), '', ' - '); ?><?php $this->options->title(); ?>">
+    <meta name="twitter:description" content="<?php $this->description(); ?>">
+    <meta name="twitter:image" content="<?php echo getPostFirstImg($this->cid); ?>">
+    
+    <!-- QQ-->
+    <meta itemprop="name" content="<?php $this->archiveTitle(array(
+                'category'  =>  _t('%s'),
+                'search'    =>  _t('%s'),
+                'tag'       =>  _t('%s'),
+                'author'    =>  _t('%s')
+            ), '', ' - '); ?><?php $this->options->title(); ?>"/>
+    <meta itemprop="image" content="<?php echo getPostFirstImg($this->cid); ?>" />
+    <meta name="description" itemprop="description" content="<?php $this->description(); ?>" />
+
+    <!-- WeChat Sharing -->
+    <script>
+     var WECHAT_TITLE = '<?php $this->archiveTitle(array(
+                'category'  =>  _t('%s'),
+                'search'    =>  _t('%s'),
+                'tag'       =>  _t('%s'),
+                'author'    =>  _t('%s')
+            ), '', ' - '); ?><?php $this->options->title(); ?>';
+     var WECHAT_DESC = '<?php $this->description(); ?>';
+     var WECHAT_IMAGE = '<?php echo getPostFirstImg($this->cid); ?>';
+     var WCHAT_NO_GA = '';
+    </script>
+
+
     <!-- 使用url函数转换相关路径 -->
     <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/bootstrap.min.css'); ?>">
     <link type="text/css" rel="stylesheet" href="<?php $this->options->themeUrl('assert/css/prism.css'); ?>">
@@ -55,13 +110,14 @@ if ($GLOBALS['style_BG'] != '') {
                 <div class="logo">
                     <div class="header-logo">
                         <!-- 标题开始 -->
-                        <span class="b">Y</span>
-                        <span class="b">U</span>
+                        <span class="b">洛</span>
+                        <span class="b">雪</span>
                         <a href="<?php $this->options->siteUrl(); ?>">
-                            <span class="w">M</span>
+                            <span class="の">M</span>
                         </a>
-                        <span class="b">O</span>
-                        <span class="b">E</span>
+                        <span class="b">C</span>
+                        <span class="b">a</span>
+                        <span class="b">t</span>
                         <!-- 标题结束 -->
                         <a id="btn-menu" href="javascript:isMenu();">
                             <span class="b">·</span>
