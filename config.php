afd32a6e14d3237615652cf7e7552fdcf4c85c74
Kevinishd <72179447+Kevinishd@users.noreply.github.com>
2025-06-18 20:36:08 +0800

    Update config.php
    修改样式，增添背景图

--- config.php
+++ config.php
@@ -6,8 +6,8 @@
 $GLOBALS['isTorTree'] = 'off'; // 默认显示文章导航树
 
 // 样式
-$GLOBALS['isAutoNav'] = 'off'; // 自动设置导航栏中 margin 及 width 值（推荐开启）
-$GLOBALS['isIconNav'] = 'off'; // 将导航栏中的 1,2,3 替换成 Emoji 图标
-$GLOBALS['isRSS'] = 'off'; // 在菜单栏中加入 RSS 按钮
+$GLOBALS['isAutoNav'] = 'on'; // 自动设置导航栏中 margin 及 width 值（推荐开启）
+$GLOBALS['isIconNav'] = 'on'; // 将导航栏中的 1,2,3 替换成 Emoji 图标
+$GLOBALS['isRSS'] = 'on'; // 在菜单栏中加入 RSS 按钮
 
-$GLOBALS['style_BG'] = ''; // 背景图设置。填入图片 URL 地址，留空为关闭
+$GLOBALS['style_BG'] = 'https://pic1.imgdb.cn/item/6256d14e239250f7c598608a.jpg'; // 背景图设置。填入图片 URL 地址，留空为关闭
